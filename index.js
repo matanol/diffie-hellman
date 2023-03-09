f91be5e1a4e80abe7a0b84c0f9027452942c7afd
matanm <matanoliel20@gmail.com>
2023-03-09 23:44:40 +0200

    removed comments

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const BASE = 16;
 
 const getKey = (generator, secret) => {
-  return BigInt(generator) ** BigInt(secret) % BigInt(p);
+    return BigInt(generator) ** BigInt(secret) % BigInt(p);
 };
 
-const p = 559784823609444184073982810118574696524917881053819639836546641442408838587276888219167938595222810044252210024146426427; // big prime number //PUBLIC
-const g = 429; // small prime number, should be less than p - 2 //PUBLIC
+const p = 559784823609444184073982810118574696524917881053819639836546641442408838587276888219167938595222810044252210024146426427; // big prime number
+const g = 429; // generator number
 
-const a = 546; // big random number //SECRET
-const b = 358; // big random number //SECRET
+const a = 546; // big random number
+const b = 358; // big random number
 
 const A = getKey(g, a); //PUBLIC
 const B = getKey(g, b); //PUBLIC
